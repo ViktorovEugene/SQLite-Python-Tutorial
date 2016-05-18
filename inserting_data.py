5a82037aef6649f6498a2a5176bcaf85d92a482f
Eugene Viktorov <eugene.viktorov@anvileight.com>
2016-05-18 18:03:59 +0300

    Refactor inserting_data.py to use executemany()

--- inserting_data.py
+++ inserting_data.py
@@ -5,15 +5,21 @@ import sqlite3 as lite
 
 con = lite.connect('test.db')
 
+cars = (
+    (1, 'Audi', 52642),
+    (2, 'Mercedes', 57127),
+    (3, 'Skoda', 9000),
+    (4, 'Volvo', 29000),
+    (5, 'Bentley', 350000),
+    (6, 'Citroen', 21000),
+    (7, 'Hummer', 41400),
+    (8, 'Volkswagen', 21600),
+)
+
 with con:
 
     cur = con.cursor()
+
+    cur.execute("DROP TABLE IF EXISTS Cars")
     cur.execute('CREATE TABLE Cars(Id INT, Name TEXT, Price INT)')
-    cur.execute("INSERT INTO Cars VALUES(1, 'Audi', 52642)")
-    cur.execute("INSERT INTO Cars VALUES(2, 'Mercedes', 57127)")
-    cur.execute("INSERT INTO Cars VALUES(3, 'Skoda', 9000)")
-    cur.execute("INSERT INTO Cars VALUES(4, 'Volvo', 29000)")
-    cur.execute("INSERT INTO Cars VALUES(5, 'Bentley', 350000)")
-    cur.execute("INSERT INTO Cars VALUES(6, 'Citroen', 21000)")
-    cur.execute("INSERT INTO Cars VALUES(7, 'Hummer', 41400)")
-    cur.execute("INSERT INTO Cars VALUES(8, 'Volkswagen', 21600)")
+    cur.executemany('INSERT INTO Cars VALUES(?, ?, ?)', cars)
