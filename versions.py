c600a57f07f44e1268dbf3af21f6704cd6f343d7
Eugene Viktorov <eugene.viktorov@anvileight.com>
2016-05-18 16:30:12 +0300

    Make script to show SQLite version

---
+++ versions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sqlite3 as lite
+import sys
+
+con = None
+
+print("Python version: %s" % sys.version.split('\n')[0])
+
+try:
+    con = lite.connect('test.db')
+
+    cur = con.cursor()
+    cur.execute('SELECT SQLITE_VERSION()')
+
+    data = cur.fetchone()
+
+    print("SQLite version: %s" % data)
+
+except lite.Error as e:
+
+    print("Error %s:" % e.args[0])
+    sys.exit(1)
+
+finally:
+
+    if con:
+        con.close()
